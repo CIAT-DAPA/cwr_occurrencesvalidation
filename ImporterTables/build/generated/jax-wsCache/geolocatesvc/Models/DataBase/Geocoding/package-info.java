4b91c25b1b092d839791bdbbff9750b97165ea82
stevensotelo <stevenbetancurt@hotmail.com>
2015-02-17 16:15:30 -0500

    refactoring code 2

--- ImporterTables/build/generated/jax-wsCache/geolocatesvc/Models/DataBase/Geocoding/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.museum.tulane.edu/webservices/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package Models.DataBase.Geocoding;
